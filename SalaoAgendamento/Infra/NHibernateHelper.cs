00685ab7b2d90536f0ae0421ccdad5cd6cbfd3c9
Andre Francisco de Oliveira <eu.andre.oliveira@hotmail.com>
2017-08-12 19:42:34 -0300

    configuração nHibernate com o mysql

---
+++ SalaoAgendamento/Infra/NHibernateHelper.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace SalaoAgendamento.Infra
+{
+    public class NHibernateHelper
+    {
+        public static Configuration Recuperaconfiguracao()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            return cfg;
+        }
+        public static void GeraSchema()
+        {
+            Configuration cfg = Recuperaconfiguracao();
+            new SchemaExport(cfg).Create(true, true);
+        }
+                
+
+    }
+
+}
